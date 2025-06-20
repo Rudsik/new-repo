e7ce91b0fa385bdc74327f71caf1fe34bd5aeed9
Rudsik <rudsika@slt.com.lk>
2025-06-20 15:13:05 +0530

    6th cmnt

--- 1.py
+++ 1.py
@@ -8,9 +8,15 @@ temperatures.append(37.0)
 temp = temperatures.pop(3)
 print(temp)
 
+# calculate average temperatures
+Average_temperature = sum(temperatures) / len(temperatures)
+
 # Find the maximum and minimum temperature in the list
 max_temperature = max(temperatures)
 min_temperature = min(temperatures)
 
+print(f"temp_list: {temperatures}")
 print(f"max_temp: {max_temperature}")
 print(f"min_temp: {min_temperature}")
+print(f"Avg_temp: {Average_temperature}")
+
